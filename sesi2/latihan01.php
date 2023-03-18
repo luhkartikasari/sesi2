0d498f5cfe680be5fad9b676366f39900e13ebe2
luhkartikasari <127809285+luhkartikasari@users.noreply.github.com>
2023-03-18 12:19:28 +0800

    upload sesi 2

---
+++ sesi2/latihan01.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Belajar Script PHP";
+    $dta = "Kartika";
+    $angka = 5;
+    echo '<br>Nama '.$dta." Peringkat ke-".$angka ;
+    echo '<br>Nama $dta'. "anak ke-". $angka;
+    echo "<br>Nama: $dta anak ke-$angka";
---
+++ sesi2/latihan03.php
@@ -0,0 +1,8 @@
+<?php
+    $kondisiawal = 3;
+
+    if($kondisiawal == 1){
+        echo "Kondisi awal sama dengan 1";
+    }else{
+        echo "Kondisi awal tidak sama dengan 1";
+    }
